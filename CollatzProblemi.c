5ea48ba0f0e0e7267043d6c7f0e3fe1f1ef5d7c2
furkanalptokac <furkanalptokac@gmail.com>
2019-10-23 10:45:49 +0300

    Edit

--- CollatzProblemi.c
+++ CollatzProblemi.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 // 1711012011 Furkan Alp Tokaç 3. Sınıf 1. Öğretim
 
 int main(){
-    int n = 0;
-    int islemID, durum;
+    int sayi = 0;
+    pid_t islemID;
  
-    printf("n sayisini giriniz: ");
-    scanf("%d", &n);
+    printf("Sayiyi giriniz: ");
+    scanf("%d", &sayi);
  
-    if(n < 0){
-        printf("0'dan buyuk bir n sayisi giriniz: ");
-        scanf("%d", &n);
+    if(sayi < 0){
+        printf("0'dan buyuk bir sayi giriniz: ");
+        scanf("%d", &sayi);
     }
  
     islemID = fork();
@@ -25,23 +27,21 @@ int main(){
  
     else if(islemID == 0){
         do{
-        if(n % 2 != 0){
-          n = (n * 3) + 1;
-        }
+        if(sayi % 2 != 0)
+          sayi = (sayi * 3) + 1;
          
-        else if(n % 2 == 0){
-          n = n / 2;
-        }
+        else if(sayi % 2 == 0)
+          sayi = sayi / 2;
  
-        printf("%d\n", n);
+        printf("%d\n", sayi);
         }
-        while(n != 1);
+        while(sayi != 1);
     }
  
     else{
         printf("pid %d \n", islemID);
         printf("Child gorev bekleniyor.\n");
-        wait(&durum);
+        wait(NULL);
     }
     
     return EXIT_SUCCESS;   
